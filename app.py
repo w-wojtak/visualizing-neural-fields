b061e6466ce9b4259b00a4c24455272a58ec9a12
weronika.wojtak <w.wojtak@gmail.com>
2023-08-21 16:56:20 +0100

    separate text areas

--- app.py
+++ app.py
@@ -31,25 +31,52 @@ col1, col2 = st.columns([1, 1])
 
 # Column 1: Text area for user input
 with col1:
-    kernel_type = st.selectbox('Choose kernel type:',
-                             ['Gaussian', 'Mex-hat', 'Oscillatory'],
-                             key='kernel_type')
+    col3, col4 = st.columns([0.3, 0.7])
+    with col3:
+        kernel_type = st.selectbox('Kernel type:',
+                                   ['Gaussian', 'Mex-hat', 'Oscillatory'],
+                                   key='kernel_type')
+    with col4:
+        # Create text area for user input based on kernel type
+        if kernel_type == 'Gaussian':
+            kernel_parameters = st.text_area("Gaussian kernel parameters:",
+                                       value=default_kernel_pars_gauss.strip(),
+                                       height=20)
+            exec(kernel_parameters, globals())
+        elif kernel_type == 'Mex-hat':
+            kernel_parameters = st.text_area("Mex-hat kernel parameters:",
+                                       value=default_kernel_pars_mex.strip(),
+                                       height=20)
+            exec(kernel_parameters, globals())
+        elif kernel_type == 'Oscillatory':
+            kernel_parameters = st.text_area("Oscillatory kernel parameters:",
+                                       value=default_kernel_pars_osc.strip(),
+                                       height=20)
+            exec(kernel_parameters, globals())
 
-    # Create text area for user input based on kernel type
-    if kernel_type == 'Gaussian':
-        kernel_pars = st.text_area("Enter Gaussian kernel parameters:", value=default_kernel_pars_gauss, height=20)
-        exec(kernel_pars, globals())
-    elif kernel_type == 'Mex-hat':
-        kernel_pars = st.text_area("Enter Mex-hat kernel parameters:", value=default_kernel_pars_mex, height=20)
-        exec(kernel_pars, globals())
-    elif kernel_type == 'Oscillatory':
-        kernel_pars = st.text_area("Enter oscillatory kernel parameters:", value=default_kernel_pars_osc, height=20)
-        exec(kernel_pars, globals())
+    with col3:
+        simulation_type = st.selectbox('Input / no input:',
+                                       ['Input', 'No input'],
+                                       key='simulation_type')
 
-    # if kernel_type:
-    #     exec(kernel_pars, globals())
+        if simulation_type == 'Input':
+            input_flag = True
+        elif simulation_type == 'No input':
+            input_flag = False
 
-    code_snippet = st.text_area("Enter your model parameters here:", value=default_code, height=300)
+    with col4:
+        if simulation_type == 'Input':
+            code_input = st.text_area("Input shape (amplitude, sigma) and position:", value=default_input.strip(), height=20)
+            exec(code_input, globals())
+            code_input_time = st.text_area("Input timing (onset time and duration):", value=default_input_time.strip(), height=20)
+            exec(code_input_time, globals())
+            initial_condition_shape = None
+        elif simulation_type == 'No input':
+            code_ic = st.text_area("Initial condition parameters: (position, amplitude, sigma)", value=default_ic.strip(), height=20)
+            exec(code_ic, globals())
+        st.write("Note: input_position, input_onset_time and input_duration must have the same length.")
+
+    code_snippet = st.text_area("Field parameters:", value=default_code.strip(), height=150)
     run_button = st.button('Run')
     simulate_message = st.empty()  # Placeholder for simulate_amari message
     if run_button:
@@ -60,7 +87,8 @@ with col1:
                 if 'field_activity' in globals():
                     if all(v is not None for v in [input_shape, input_position, input_onset_time, input_duration]):
                         input_pars = [input_shape, input_position, input_onset_time, input_duration]
-                        field_activity, inputs = simulate_amari(kernel_type, field_pars, kernel_pars, input_flag, input_pars,
+                        field_activity, inputs = simulate_amari(kernel_type, field_pars, kernel_parameters, input_flag,
+                                                                input_pars,
                                                                 initial_condition_shape)
                         st.session_state.simulation_results['field_activity'] = field_activity
                         st.session_state.simulation_results['inputs'] = inputs
--- src/default_values.py
+++ src/default_values.py
@@ -1,30 +1,32 @@
 default_code = """
-
-# Field parameters
 x_lim, t_lim = 10, 15  # Limits for space and time. Space is set as [-x_lim, x_lim], time as [0, t_lim].
 dx, dt = 0.0025, 0.01  # Spatial and temporal discretization.
 theta = 0.1  # Threshold for the activation function.
 field_pars = [x_lim, t_lim, dx, dt, theta]
-
-# Input parameters
-input_flag = True  # Flag indicating if inputs are present.
-input_shape = [2.5, 0.5]  # parameters of gaussian inputs: amplitude, sigma.
-input_position = [-2]  # input_position, input_onset_time and input_duration must have the same length.
-input_onset_time = [2]
-input_duration = [1]
-
-# Initial condition - gaussian (if input_flag = False).
-initial_condition_shape = [0, 0.5, 0.2]  # position, amplitude, sigma
 """
 
-default_kernel_pars_mex = """
-kernel_pars = [1.3, 0.4, 0.5, 0.5, 0.15]
+default_ic = """
+initial_condition_shape = [0, 0.5, 0.2] 
+"""
+
+default_input = """
+input_shape = [2.5, 0.5] 
+input_position = [-2]
+"""
+
+default_input_time = """
+input_onset_time = [2]
+input_duration = [1]
+"""
+
+default_kernel_pars_mex = """ 
+kernel_parameters = [1.3, 0.4, 0.5, 0.5, 0.15]
 """
 
 default_kernel_pars_gauss = """
-kernel_pars = [1, 0.4, 0.2]
+kernel_parameters = [1, 0.4, 0.2]
 """
 
 default_kernel_pars_osc = """
-kernel_pars = [1, 0.3, 0.9]
+kernel_parameters = [1, 0.3, 0.9]
 """
